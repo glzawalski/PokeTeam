78c92db8fed4d2c469b4c9ab24acd0179266a55a
Gabriel Zawalski <gabrielzawalski@gmail.com>
2022-05-21 17:00:50 -0300

    init new project

---
+++ PokeTeam App/PokeTeam App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Levis Zawalski on 21/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
