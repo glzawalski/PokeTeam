8175790388c305ba7b17a7dfd4698699c291feae
Gabriel Zawalski <gabrielzawalski@gmail.com>
2022-05-21 18:50:49 -0300

    Create network models for fetching type data

---
+++ PokeTeam App/PokeTeam App/Network/Models/PokemonType.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonType.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Levis Zawalski on 21/05/22.
+//
+
+struct PokemonType: Decodable {
+    var name: String
+    var url: String
+}
---
+++ PokeTeam App/PokeTeam App/Network/Models/PokemonTypeList.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonTypeList.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Levis Zawalski on 21/05/22.
+//
+
+struct PokemonTypeList: Decodable {
+    var count: Int
+    var results: [PokemonType]
+}
