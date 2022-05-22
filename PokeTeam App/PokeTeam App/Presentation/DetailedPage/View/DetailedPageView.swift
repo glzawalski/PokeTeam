ea4defd5312a5b75d40189d656bd4ecb905072b7
Gabriel Zawalski <gabrielzawalski@gmail.com>
2022-05-21 21:51:01 -0300

    Make detailed page view

---
+++ PokeTeam App/PokeTeam App/Presentation/DetailedPage/View/DetailedPageView.swift
@@ -0,0 +1,101 @@
+//
+//  DetailedPageView.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Levis Zawalski on 21/05/22.
+//
+
+import UIKit
+
+class DetailedPageView: UIViewController {
+    
+    var selectedPokemon: String = ""
+    
+    private lazy var idLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var heightLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var weightLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var viewModel: DetailedPageViewModelInput = {
+        return DetailedPageViewModel(output: self)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        addComponents()
+        addConstraints()
+        viewModel.fetchData(for: selectedPokemon)
+    }
+    
+    private func configure() {
+        guard let model = viewModel.model else { return }
+        
+        idLabel.text = model.id
+        nameLabel.text = model.name
+        heightLabel.text = model.height
+        weightLabel.text = model.weight
+        
+    }
+}
+
+// MARK: - COMPONENTS AND CONSTRAINTS
+extension DetailedPageView {
+    
+    private func addComponents() {
+        view.addSubview(idLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(heightLabel)
+        view.addSubview(weightLabel)
+    }
+    
+    private func addConstraints() {
+        idLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        idLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        nameLabel.topAnchor.constraint(equalTo: idLabel.bottomAnchor, constant: 10).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        heightLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10).isActive = true
+        heightLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        weightLabel.topAnchor.constraint(equalTo: heightLabel.bottomAnchor, constant: 10).isActive = true
+        weightLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    }
+}
+
+// MARK: - VIEW MODEL OUTPUT
+extension DetailedPageView: DetailedPageViewModelOutput {
+    
+    func didFetchData() {
+        DispatchQueue.main.async {
+            self.configure()
+        }
+    }
+    
+    func didFailFetchData() {
+        DispatchQueue.main.async {
+            self.configure()
+        }
+    }
+}
+
