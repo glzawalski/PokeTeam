549830843f22dba772131c8c430f3e89f9f3cde8
Gabriel Zawalski <gabrielzawalski@gmail.com>
2022-05-21 21:49:12 -0300

    Make pokemon details to fetch data from api

---
+++ PokeTeam App/PokeTeam App/Network/Models/PokemonDetails.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonDetails.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Levis Zawalski on 21/05/22.
+//
+
+struct PokemonDetails: Decodable {
+    
+    var id: Int
+    var name: String
+    var height: Int
+    var weight: Int
+}
