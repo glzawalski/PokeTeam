5e6944c0817fe5bcf7b8bb718763105396cf2314
Gabriel Zawalski <gabrielzawalski@gmail.com>
2022-05-22 21:05:51 -0300

    Create pokemon details type

---
+++ PokeTeam App/PokeTeam App/Network/Models/PokemonDetailsType.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonDetailsType.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Levis Zawalski on 22/05/22.
+//
+
+struct PokemonDetailsType: Decodable {
+    var slot: Int
+    var type: PokemonTypeListItem
+}
