ec1149b5e06d302186334da78c8c315f98dffc8b
Gabriel Zawalski <gabrielzawalski@gmail.com>
2022-05-25 22:30:11 -0300

    Create team builder model

---
+++ PokeTeam App/PokeTeam App/Presentation/Team/Model/TeamModel.swift
@@ -0,0 +1,12 @@
+//
+//  TeamModel.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Levis Zawalski on 25/05/22.
+//
+
+final class TeamModel {
+    
+    var pokemons: [Int: String] = [:]
+    let limit: Int = 6
+}
