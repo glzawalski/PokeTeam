77f719746a25143f2e92cd07f700e1e54d6f30c0
glzawalski <gabrielzawalski@gmail.com>
2022-08-22 22:41:49 -0300

    Creates new HomeView in SwiftUI

---
+++ PokeTeam App/PokeTeam App/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  PokeTeam App
+//
+//  Created by Gabriel Zawalski on 22/08/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var viewModel: HomeViewModel
+
+    let defaults = UserDefaults.standard
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.model?.types ?? []) { type in
+                HStack {
+                    Text(type.name)
+                }
+            }
+        }
+        .onAppear(perform: viewModel.fetchData)
+    }
+}
+
+// TODO: Add scroll to favorite type on list
+// TODO: defaults.set(typeName, forKey: "FavoriteType")
+// TODO: Highlight favorite type
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .environmentObject(HomeViewModel())
+    }
+}
